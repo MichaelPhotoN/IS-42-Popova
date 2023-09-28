ab8deb9dcaaa79be6152325c445480bfb0f1f194
MichaelPnotoN <96090081+MichaelPnotoN@users.noreply.github.com>
2023-09-28 14:13:16 +0300

    Add files via upload

---
+++ 5_main.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <climits>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int n;
+    // Запросить у пользователя размер массива
+    std::cout << "Введите размер массива: ";
+    std::cin >> n;
+    if (n <= 0) {
+        std::cout << "Размер массива должен быть положительным числом." << std::endl;
+        return 1;
+    }
+    // Выделяем память под динамический массив
+    int* arr = new int[n];
+    // Заполнить массив элементами
+    std::cout << "Введите элементы массива:" << std::endl;
+    for (int i = 0; i < n; i++) {
+        std::cin >> arr[i];
+    }
+    // Находим минимальный элемент и его индекс
+    int minElement = INT_MAX;
+    int minIndex = -1;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] < minElement) {
+            minElement = arr[i];
+            minIndex = i;
+        }
+    }
+    // Проверяем, что минимальный элемент был найден
+    if (minIndex == -1) {
+        std::cout << "Минимальный элемент не найден." << std::endl;
+        delete[] arr; // Освобождаем выделенную память
+        return 1;
+    }
+    // Вычисляем сумму элементов после минимального элемента
+    int sum = 0;
+    for (int i = minIndex + 1; i < n; i++) {
+        sum += arr[i];
+    }
+    // Выводим результат
+    std::cout << "Сумма элементов после минимального элемента: " << sum << std::endl;
+
+    // Освобождаем выделенную память
+    delete[] arr;
+    return 0;
+}
---
+++ 5_tests.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <climits>
+#include <locale>
+
+// Функция для поиска минимального элемента и его индекса
+int findMinElementIndex(int arr[], int n) {
+    int minIndex = 0;
+    int minElement = arr[minIndex];
+    for (int i = 1; i < n; i++) {
+        if (arr[i] < minElement) {
+            minElement = arr[i];
+            minIndex = i;
+        }
+    }
+    return minIndex;
+}
+
+// Функция для вычисления суммы элементов после минимального элемента
+int sumElementsAfterMin(int arr[], int n) {
+    int minIndex = findMinElementIndex(arr, n);
+    int sum = 0;
+    for (int i = minIndex + 1; i < n; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+void runTest(int arr[], int n, int expectedSum) {
+    if (n <= 0) {
+        std::cout << "Размер массива должен быть положительным числом." << std::endl;
+    }
+    else {
+
+        int actualSum = sumElementsAfterMin(arr, n);
+        if (actualSum == expectedSum) {
+            std::cout << "Тест пройден." << std::endl;
+        }
+        else {
+            std::cout << "Тест не пройден. Ожидаемая сумма: " << expectedSum
+                << ", Фактическая сумма: " << actualSum << std::endl;
+        }
+
+    }
+}
+
+int main() {
+    // Устанавливаем локаль для поддержки русского языка
+    setlocale(LC_ALL, "Russian");
+
+    int arr1[] = { 10, 20, 30, 40, 50 };
+    int n1 = 5;
+    int expectedSum1 = 140;
+    runTest(arr1, n1, expectedSum1);
+
+    int arr2[] = { -30, -5, -10, -15, -20, -25 };
+    int n2 = 6;
+    int expectedSum2 = -75;
+    runTest(arr2, n2, expectedSum2);
+
+    int arr3[] = { 0, 0, 0, 0 };
+    int n3 = 4;
+    int expectedSum3 = 0;
+    runTest(arr3, n3, expectedSum3);
+
+    int arr4[] = { };
+    int n4 = 0;
+    int expectedSum4 = 0;
+    runTest(arr4, n4, expectedSum4);
+
+    return 0;
+}
